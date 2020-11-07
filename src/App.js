8bddeb60b74f6ddd296694e1b15b1ec7725eea21
AlexButeau <alexbuteau1@gmail.com>
2020-11-07 11:06:01 +0100

    created a first version of the routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import Contact from './Contact';
+import Product from './Product';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/contact' component={Contact} />
+          <Route path='/:id' component={Product} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
