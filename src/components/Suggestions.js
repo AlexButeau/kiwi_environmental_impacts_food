a0b948cfea6ffc2c17b2fe1f916c9e8c2f2f6cab
AlexButeau <alexbuteau1@gmail.com>
2020-11-12 20:39:53 +0100

    test

--- src/components/Suggestions.js
+++ src/components/Suggestions.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ProductCard from './ProductCard';
+import './styles/suggestions.scss';
 
 const Suggestions = () => {
-  const suggestions = []; //will be an array of objects, each object with at least product name and product id
+  const suggestions = [
+    { name: 'Pizza margarita', id: 25404 },
+    { name: 'Boeuf bourguinon', id: 25033 },
+    { name: 'Lasagnes légumes-chèvre', id: 25219 },
+    { name: 'Salade César', id: 25628 },
+  ]; //will be an array of objects, each object with at least product name and product id
   return (
     <div className="suggestions-wrapper">
       {suggestions.map((product) => {
