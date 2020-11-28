4c45e791504270a3cd25ba9fb079431000174170
AlexButeau <alexbuteau1@gmail.com>
2020-11-28 12:00:23 +0100

    first working version of the search bar, he results are sorted by relevance, still a lot to css to do

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,10 +1,13 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React, { useContext, useEffect, useState } from 'react';
 import { ApiDataContext } from './contexts/ApiDataContext';
+import './styles/searchbar.scss';
 
 const Searchbar = () => {
   const { apiData, fetchApiDataQuery } = useContext(ApiDataContext);
   const [query, setQuery] = useState('');
-  const [suggestionsList, setSuggestionsList] = useState([]);
+  const [suggestionsList, setSuggestionsList] = useState();
 
   const handleChange = (e) => {
     setQuery(() => e.target.value);
@@ -17,19 +20,65 @@ const Searchbar = () => {
     const arraySuggestions = apiData.map(
       (item) => item.results[0]['Nom_du_Produit_en_Français'],
     );
-    setSuggestionsList(arraySuggestions);
+    const sortedArray = arraySuggestions.sort((a, b) => {
+      if (
+        a.toLowerCase().indexOf(query) >= 0 &&
+        b.toLowerCase().indexOf(query) >= 0
+      ) {
+        return a.toLowerCase().indexOf(query) - b.toLowerCase().indexOf(query);
+      }
+      if (
+        a.toLowerCase().indexOf(query) < 0 &&
+        b.toLowerCase().indexOf(query) < 0
+      ) {
+        return 0;
+      }
+      return b.toLowerCase().indexOf(query) === 0
+        ? 1
+        : b.toLowerCase().indexOf(query);
+    });
+    setSuggestionsList(sortedArray);
   }, [apiData]);
 
   return (
-    <div className="searchbar">
+    <div id="search-bar">
       <form>
-        <input
-          type="text"
-          className="query"
-          name="query"
-          value={query}
-          onChange={(e) => handleChange(e)}
-        />
+        <div className="autocomplete">
+          <label htmlFor="query">
+            {' '}
+            Entrez un plat/aliment
+            <input
+              type="text"
+              className="query"
+              name="query"
+              value={query}
+              onChange={(e) => handleChange(e)}
+            />
+          </label>
+          <div id="autocomplete-list" className="autocomplete-items">
+            {suggestionsList &&
+              // showList &&
+              suggestionsList.map((item) => (
+                <div
+                  key={item}
+                  // onClick={(e) => {
+                  // submitButton.current.focus();
+                  // setShowList(false);
+                  // setAddress((prevValue) => ({
+                  //   ...prevValue,
+                  //   street: e.target.innerHTML,
+                  //   coordinates: item.geometry.coordinates,
+                  // }));
+                  // setAutocomplete(null);
+                  // setEnableSubmit(true);
+                  // }}
+                >
+                  {item}
+                </div>
+              ))}
+          </div>
+        </div>
+
         <input type="submit" value="Valider" />
       </form>
     </div>
