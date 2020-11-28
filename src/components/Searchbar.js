1a58e5c2e77f058403a35c5de7cf262a9565129a
AlexButeau <alexbuteau1@gmail.com>
2020-11-28 16:53:22 +0100

    working version of the search bar but still have to check for issues with focus, submit, force selection before submit

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,48 +1,102 @@
+/* eslint-disable object-curly-newline */
+/* eslint-disable function-paren-newline */
+/* eslint-disable implicit-arrow-linebreak */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState, useDebugValue } from 'react';
+import { Link, withRouter } from 'react-router-dom';
 import { ApiDataContext } from './contexts/ApiDataContext';
 import './styles/searchbar.scss';
 
-const Searchbar = () => {
-  const { apiData, fetchApiDataQuery } = useContext(ApiDataContext);
-  const [query, setQuery] = useState('');
-  const [suggestionsList, setSuggestionsList] = useState();
+function useStateWithLabel(initialValue, name) {
+  const [value, setValue] = useState(initialValue);
+  useDebugValue(`${name}: ${value}`);
+  return [value, setValue];
+}
+
+export function Searchbar() {
+  const { apiDataQuery, fetchApiDataQuery } = useContext(ApiDataContext);
+  const [query, setQuery] = useStateWithLabel('', 'query');
+  const [selection, setSelection] = useStateWithLabel('', 'selected item');
+  const [showList, setShowList] = useStateWithLabel(false, 'show list');
+  const [suggestionsList, setSuggestionsList] = useStateWithLabel(
+    [],
+    'suggestionsList',
+  );
 
   const handleChange = (e) => {
+    console.log(e.target.value);
     setQuery(() => e.target.value);
-    if (e.target.value.length > 0) {
-      fetchApiDataQuery(e.target.value.split(' ').join('+'));
-    }
   };
 
   useEffect(() => {
-    const arraySuggestions = apiData.map(
-      (item) => item.results[0]['Nom_du_Produit_en_Français'],
-    );
+    if (query.length > 1) {
+      fetchApiDataQuery(query);
+    }
+  }, [query]);
+
+  useEffect(() => {
+    const queryTerms = query
+      .split(' ')
+      .filter((term) => term.length > 2)
+      .map((term) => term.toLowerCase()); // provides with an array of the words
+
+    // console.log(
+    //   apiData.map((item) => item.results[0]['Nom_du_Produit_en_Français']),
+    // );
+    const arraySuggestions = apiDataQuery
+      .map((item) => ({
+        name: item.results[0]['Nom_du_Produit_en_Français'],
+        id: item.value,
+      }))
+      .filter((item) => {
+        for (let i = 0; i < queryTerms.length; i += 1) {
+          if (item.name.toLowerCase().includes(queryTerms[i])) {
+            return true;
+          }
+        }
+        return false;
+      });
     const sortedArray = arraySuggestions.sort((a, b) => {
       if (
-        a.toLowerCase().indexOf(query) >= 0 &&
-        b.toLowerCase().indexOf(query) >= 0
+        a.name.toLowerCase().indexOf(query) >= 0 &&
+        b.name.toLowerCase().indexOf(query) >= 0
       ) {
-        return a.toLowerCase().indexOf(query) - b.toLowerCase().indexOf(query);
+        return (
+          a.name.toLowerCase().indexOf(query) -
+          b.name.toLowerCase().indexOf(query)
+        );
       }
       if (
-        a.toLowerCase().indexOf(query) < 0 &&
-        b.toLowerCase().indexOf(query) < 0
+        a.name.toLowerCase().indexOf(query) < 0 &&
+        b.name.toLowerCase().indexOf(query) < 0
       ) {
         return 0;
       }
-      return b.toLowerCase().indexOf(query) === 0
-        ? 1
-        : b.toLowerCase().indexOf(query);
+      if (
+        a.name.toLowerCase().indexOf(query) >= 0 &&
+        b.name.toLowerCase().indexOf(query) < 0
+      ) {
+        return -1;
+      }
+      if (
+        a.name.toLowerCase().indexOf(query) < 0 &&
+        b.name.toLowerCase().indexOf(query) >= 0
+      ) {
+        return 1;
+      }
+      return 0;
     });
     setSuggestionsList(sortedArray);
-  }, [apiData]);
+  }, [apiDataQuery]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <div id="search-bar">
-      <form>
+      <form onSubmit={(e) => handleSubmit(e)}>
         <div className="autocomplete">
           <label htmlFor="query">
             {' '}
@@ -53,36 +107,54 @@ const Searchbar = () => {
               name="query"
               value={query}
               onChange={(e) => handleChange(e)}
+              onFocus={() => {
+                setShowList(true);
+              }}
             />
           </label>
           <div id="autocomplete-list" className="autocomplete-items">
             {suggestionsList &&
-              // showList &&
-              suggestionsList.map((item) => (
+              showList &&
+              suggestionsList.slice(0, 10).map((item) => (
                 <div
-                  key={item}
-                  // onClick={(e) => {
-                  // submitButton.current.focus();
-                  // setShowList(false);
-                  // setAddress((prevValue) => ({
-                  //   ...prevValue,
-                  //   street: e.target.innerHTML,
-                  //   coordinates: item.geometry.coordinates,
-                  // }));
-                  // setAutocomplete(null);
-                  // setEnableSubmit(true);
-                  // }}
+                  key={item.name}
+                  className={
+                    item.name.toLowerCase().includes(query) ? 'relevant' : ''
+                  }
+                  onClick={() => {
+                    // submitButton.current.focus();
+                    // setShowList(false);
+                    // setAddress((prevValue) => ({
+                    //   ...prevValue,
+                    //   street: e.target.innerHTML,
+                    //   coordinates: item.geometry.coordinates,
+                    // }));
+                    setSelection(item);
+                    setQuery(item.name);
+                    // setSuggestionsList(null);
+                    setShowList(false);
+                    // setEnableSubmit(true);
+                  }}
                 >
-                  {item}
+                  {item.name}
                 </div>
               ))}
           </div>
         </div>
 
-        <input type="submit" value="Valider" />
+        <Link
+          to={`/${selection.id}`}
+          onClick={() => {
+            console.log('coucou');
+            setShowList(false);
+          }}
+        >
+          {' '}
+          <input type="submit" value="Valider" />{' '}
+        </Link>
       </form>
     </div>
   );
-};
+}
 
-export default Searchbar;
+export default withRouter(Searchbar);
